c65841b48458c9db228fb7ef7674f97a8c3073a9
SergiByte92 <sergigs92@gmail.com>
2026-03-05 13:46:43 +0100

    Fix: assetPrefix relativo para Android/Capacitor

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
+  
 };
 
 export default nextConfig;
